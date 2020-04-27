04286b9618c6b045b21693153f72b9644e321005
Vitalii __ <dev@deerhead.net>
2020-04-27 14:13:13 +0300

    Add first style

--- src/app/app.js
+++ src/app/app.js
@@ -29,17 +29,70 @@ export default class App extends Component {
                         </div>
                         <div className="section-content">
                             <div className="box-title">
-                                <h4>
-                                    Question: How to set ripple color for lconButton?
-                                </h4>
-                                <span className="tally">Question</span>
+                                <a href="#">
+                                    <h4>
+                                        Question: How to set ripple color for lconButton?
+                                    </h4>
+                                </a>
+                                <span className="tally -question">Question</span>
                             </div>
                             <div className="box-data">
                                 #616 opened 15 hours ago by mam
                             </div>
                         </div>
                         <div className="section-notification">
-                            <i className="fa fa-comment"></i>
+                            <button type="button" className="btn custom ">
+                                <i className="fa fa-comment"></i>
+                            </button>
+                            <span>5</span>
+                        </div>
+                    </div>
+                    <div className="todo-list-item">
+                        <div className="section-icon-status">
+                            <i className="fa fa-exclamation-circle"></i>
+                        </div>
+                        <div className="section-content">
+                            <div className="box-title">
+                                <a href="#">
+                                    <h4>
+                                        Migration to v6 broke the SASS compilation
+                                    </h4>
+                                </a>
+                            </div>
+                            <div className="box-data">
+                                #615 opened 4 days ago by BenMoreton
+                            </div>
+                        </div>
+                        <div className="section-notification">
+                            <button type="button" className="btn custom ">
+                                <i className="fa fa-comment"></i>
+                            </button>
+                            <span>5</span>
+                        </div>
+                    </div>
+                    <div className="todo-list-item">
+                        <div className="section-icon-status">
+                            <i className="fa fa-exclamation-circle"></i>
+                        </div>
+                        <div className="section-content">
+                            <div className="box-title">
+                                <a href="#">
+                                    <h4>
+                                        Navigation Drawer List Item Icon color doesn't use theme color when in active
+                                        state,
+                                        differs from the text color.
+                                        <span className="tally -fixed">Fixed in Upcoming Release</span>
+                                    </h4>
+                                </a>
+                            </div>
+                            <div className="box-data">
+                                #602 opened 17 days ago by kintz09
+                            </div>
+                        </div>
+                        <div className="section-notification">
+                            <button type="button" className="btn custom ">
+                                <i className="fa fa-comment"></i>
+                            </button>
                             <span>5</span>
                         </div>
                     </div>
